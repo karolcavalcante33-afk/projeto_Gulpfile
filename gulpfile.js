574b02166aa2c799005b79f0855ca5da66cc448b
karolcavalcante33-afk <karolcavalcante33@gmail.com>
2026-02-05 18:49:43 -0300

    Configuração do Gulp com SASS, JS e imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
-const gulp = require('gulp')
-const imagemin = require('gulp-imagemin')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
-function comprimirImagens() {
-    return gulp.src('src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+// SASS → CSS
+function compilaSass() {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.comprimirImagens = comprimirImagens
+// JS
+function comprimeJs() {
+  return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
+
+// Imagens
+function comprimeImagens() {
+  return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+// ✅ TASK DEFAULT (OBRIGATÓRIA)
+exports.default = gulp.series(
+  compilaSass,
+  comprimeJs,
+  comprimeImagens
+);
